acf138c0a26f2988dd37862305e8d47b0fbecb7c
nsdeschenes <nsdeschenes@outlook.com>
2022-08-24 07:46:48 -0300

    use chakra button props for AxeButton

--- frontend/src/components/AxeButton.tsx
+++ frontend/src/components/AxeButton.tsx
@@ -1,12 +1,16 @@
-import { Button } from "@chakra-ui/react";
+import { Button, ButtonProps } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
-const AxeButton: React.FC<PropsWithChildren> = ({ children }) => {
+const AxeButton: React.FC<PropsWithChildren<ButtonProps>> = ({
+  children,
+  ...props
+}) => {
   return (
     <Button
       backgroundColor="burlywood"
       textColor="darkred"
       _hover={{ backgroundColor: "darkred", textColor: "burlywood" }}
+      {...props}
     >
       {children}
     </Button>
